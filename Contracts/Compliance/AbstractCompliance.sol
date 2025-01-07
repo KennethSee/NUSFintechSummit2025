fbc0c3ae068bcfa45f3b1e98289f11b8b498fa3e
KennethSee <kennethseede@gmail.com>
2025-01-08 01:49:59 +0800

    add ability to stack contracts

--- Contracts/Compliance/AbstractCompliance.sol
+++ Contracts/Compliance/AbstractCompliance.sol
@@ -2,6 +2,52 @@
 pragma solidity ^0.8.0;
 
 abstract contract AbstractCompliance {
-    function softCheck(address _payer, address _recipient, uint256 _amount, string memory _payerCountry, string memory _recipientCountry, address _countriesContract) public view virtual returns (bool);
-    function hardCheck(address _payer, address _recipient, uint256 _amount, string memory _payerCountry, string memory _recipientCountry, address _countriesContract) public view virtual returns (bool);
+    uint256 public startTime;
+    uint256 public endTime;
+    address public previousComplianceContract;
+    address public nextComplianceContract;
+    
+    function setPreviousComplianceContract(address _previous) external virtual {
+        previousComplianceContract = _previous;
+    }
+
+    function addNewComplianceContract(address _newComplianceContractAddress) public virtual {
+        AbstractCompliance newComplianceContract = AbstractCompliance(_newComplianceContractAddress);
+        require(newComplianceContract.startTime() > startTime, "New compliance contract must start later than this.");
+
+        endTime = block.timestamp;
+        nextComplianceContract = _newComplianceContractAddress;
+        newComplianceContract.setPreviousComplianceContract(address(this));
+    }
+
+    function getLatestComplianceContract() public view virtual returns(address) {
+        address latestContract;
+        if (nextComplianceContract != address(0)) {
+            AbstractCompliance complianceContract = AbstractCompliance(nextComplianceContract);
+            latestContract = complianceContract.getLatestComplianceContract();
+        }
+        else {
+            latestContract = address(this);
+        }
+        return latestContract;
+    }
+
+    function softCheck(
+        address _payer,
+        address _recipient,
+        uint256 _amount,
+        string memory _payerCountry,
+        string memory _recipientCountry,
+        address _countriesContract
+    ) public view virtual returns (bool);
+
+    function hardCheck(
+        address _payer,
+        address _recipient,
+        uint256 _amount,
+        string memory _payerCountry,
+        string memory _recipientCountry,
+        address _countriesContract
+    ) public view virtual returns (bool);
+
 }
--- Contracts/Compliance/Compliance20250107.sol
+++ Contracts/Compliance/Compliance20250107.sol
@@ -4,13 +4,39 @@ import "./AbstractCompliance.sol";
 pragma solidity ^0.8.0;
 
 contract Compliance20250107 is AbstractCompliance {
-
-    function softCheck(address _payer, address _recipient, uint256 _amount, string memory _payerCountry, string memory _recipientCountry, address _countriesContract) public view override returns (bool) {
-        return false;
+    constructor() {
+        startTime = block.timestamp;
     }
 
-    function hardCheck(address _payer, address _recipient, uint256 _amount, string memory _payerCountry, string memory _recipientCountry, address _countriesContract) public view override returns (bool) {
-        return true;
+    function softCheck(
+        address _payer,
+        address _recipient,
+        uint256 _amount,
+        string memory _payerCountry,
+        string memory _recipientCountry,
+        address _countriesContract
+    ) public view override returns (bool) {
+        // Recursive retrieval of value if there is a more recent Compliance contract
+        if (nextComplianceContract != address(0)) {
+            AbstractCompliance newCompliance = AbstractCompliance(nextComplianceContract);
+            return newCompliance.softCheck(_payer, _recipient, _amount, _payerCountry, _recipientCountry, _countriesContract);
+        }
+        return false; // Default behavior if nextComplianceContract is not set
     }
 
+    function hardCheck(
+        address _payer,
+        address _recipient,
+        uint256 _amount,
+        string memory _payerCountry,
+        string memory _recipientCountry,
+        address _countriesContract
+    ) public view override returns (bool) {
+        // Recursive retrieval of value if there is a more recent Compliance contract
+        if (nextComplianceContract != address(0)) {
+            AbstractCompliance newCompliance = AbstractCompliance(nextComplianceContract);
+            return newCompliance.hardCheck(_payer, _recipient, _amount, _payerCountry, _recipientCountry, _countriesContract);
+        }
+        return false; // Default behavior if nextComplianceContract is not set
+    }
 }

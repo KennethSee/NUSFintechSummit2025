c2a08b40d908d78b59332551fb607a1ef6686c10
baymaxXueWen <tanxuewen10@gmail.com>
2025-01-10 00:54:11 +0800

    streamline

--- Fintech_hackathon/readme.md
+++
@@ -1,2 +0,0 @@
-bash run_nlp.sh ./demo_docs/FinCENgov.pdf
-bash run_nlp.sh ./demo_docs/MASDoc.pdf
--- Fintech_hackathon/run_bash.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Path to the bash file
-bash_script_path = "./script.sh"
-
-# Run the bash file
-exit_code = os.system(f"bash run_nlp.sh demo_docs/FinCENgov.pdf")
-
-print("Exit Code:", exit_code)
